10b58af3d76d9c20dc652907d9e60fd255ffe0d6
Jeetu singh <xtaraim@gmail.com>
2018-05-13 19:54:14 +0530

    Add files via upload
    crptocurrency  api  full code

--- lookup.py
+++ lookup.py
@@ -1,58 +1,11 @@
 import requests
- import json
- import os
- os.system('cls')
+import json
+import os
+os.system('cls')
+#########################################
 
-#####################################
- api_request = requests.get("https"://api.coinmarketcap.com/v1/ticker/")
- for x in api:
-	for coin in currencies:
-	    if coin == x["symbol"]:
-	       print(x["name"])
-         print(x["price_usd"])
-         print(x["rank"])
-         print("${0:.2f}".format(float(x["price_usd"])))
-	       print("Rank:{0:.0f}".format(float(x["rank"])))
-	       print("==========================")
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         ####################################################################################
-         	       
-1
-3
-6
-29
-Bitcoin
-$8516.86
-Rank:1
-==========================
-Ripple
-$0.64
-Rank:3
-==========================
-EOS
-$6.78
-Rank:6
-==========================
-Steem
-$1.99
-Rank:29
-==========================
+
+api_request = requests.get( "https://api.coinmarketcap.com/v1/ticker/")
+api = json.loads(api_request.content)
+
+print(api)
